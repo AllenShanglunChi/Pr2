4230c84a320c6f75e2f0961d657f5674224ee5f3
allen <allen@WANYUANJI>
2023-12-08 20:52:48 +1100

    Add project files.

---
+++ AddStaffProfilePage.xaml.cs
@@ -0,0 +1,65 @@
+using Microsoft.Maui.Controls;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Pr2
+{
+    public partial class AddStaffProfilePage : ContentPage
+    {
+        private ObservableCollection<Department> _departments;
+        private MainPageViewModel _viewModel; // Add a reference to MainPageViewModel
+
+        public AddStaffProfilePage(ObservableCollection<Department> departments, MainPageViewModel viewModel)
+        {
+            InitializeComponent();
+            _departments = departments;
+            _viewModel = viewModel; // Assign the instance of MainPageViewModel
+
+            BindingContext = viewModel;
+
+            PopulateDepartments();
+        }
+
+        public AddStaffProfilePage(MainPageViewModel bindingContext)
+        {
+            BindingContext = bindingContext;
+        }
+
+        private void PopulateDepartments()
+        {
+            foreach (var department in _departments)
+            {
+                departmentPicker.Items.Add(department.Name);
+            }
+
+            if (_departments.Any())
+            {
+                departmentPicker.SelectedIndex = 0;
+            }
+        }
+
+        private void OnSaveClicked(object sender, EventArgs e)
+        {
+            var newStaff = new Staff
+            {
+                Name = nameEntry.Text,
+                Phone = phoneEntry.Text,
+                DepartmentId = _departments[departmentPicker.SelectedIndex].Id,
+                Address = addressEntry.Text
+            };
+
+            // Validate and save the new staff profile to the backend or local storage
+            // For demo purposes, add the new staff to the collection
+
+            _viewModel.StaffList.Add(newStaff);
+
+            Navigation.PopAsync();
+        }
+
+        private void OnCancelClicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿
+using Microsoft.Maui.Controls;
+using System.Diagnostics;
+
+namespace Pr2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainPageViewModel();
+            Console.WriteLine("MainPage constructor called");
+        }
+
+        private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            Debug.WriteLine("OnItemSelected executed");
+
+            if (e.SelectedItem == null)
+                return;
+
+            var selectedStaff = e.SelectedItem as Staff;
+            Navigation.PushAsync(new StaffProfileDetailsPage(selectedStaff, (MainPageViewModel)BindingContext));
+
+            // Deselect item
+            ((ListView)sender).SelectedItem = null;
+        }
+
+        private void OnAddClicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine("OnAddClicked executed");
+            // Handle the Add button click event
+            Navigation.PushAsync(new AddStaffProfilePage((MainPageViewModel)BindingContext));
+        }
+    }
+}
